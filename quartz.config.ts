b206702e9506ba2433aacbd22e34e5b030446817
onlysun <onlysun79@gmail.com>
2026-04-23 16:41:56 +0900

    공통수정

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "webdogu",
+    pageTitle: "웹도구",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -8,19 +8,11 @@ interface Options {
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          <a href="http://webdogu.com/">WebDogu</a> © 2013-{year}. All rights reserved.
+          <a href="http://webdogu.com/">webdogu</a> © 2013-{year}. All rights reserved.
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
       </footer>
     )
   }
